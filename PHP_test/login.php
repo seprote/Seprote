d75ac275604b7e701462be9f4fa77355851ca70e
yaziid <cantinanv@hotmail.com>
2016-12-06 15:54:44 +0100

    Add files via upload

--- PHP_test/login.php
+++ PHP_test/login.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+<!doctype html>
 <html>
 <head>
 	<meta charset="utf-8"/>
@@ -21,66 +23,74 @@
 	<?php
 
 	require_once "BDD.php";
-
-	if(empty($_POST['login']) || empty($_POST['mdp'])){
-				?>
-			
-		<form method="POST" action="login.php" id="loginForm">
-			<label>Nom d'utilisateur</label>
-			<input type="text" name="login"/>
-			<br/><br/>
-			<label>Mot de passe</label>
-			<input type="password" name="mdp"/>
-			<br/><br/><br/>
-			<button type="input">Connexion</button>
-		</form>
-	<?php
-
-		if(isset($_POST['login']) || isset($_POST['mdp'])) 
-		{
-			?>
-			<div class="ui-widget">
-				<div class="ui-state-error ui-corner-all" style="padding: 0 .7em;"> 
-					<p>
-						<span class="ui-icon ui-icon-alert" 
-						    style="margin-right: .3em;"></span>
-						<strong>ERREUR: </strong> L'un des 2 champs est vide !
-					</p>
-				</div>
-			</div>
-			<?php
-		}
-	}
-	else{
-		$req = $bdd->prepare('SELECT id_u, mail, mdp, nom, prenom FROM utilisateur WHERE mail = :mail AND mdp = :mdp');
-		$req->execute(array(
-		'mail' => $_POST['login'], 
-		'mdp' => sha1($_POST['mdp'])
-		));
-
-		$resultat = $req->fetch();
-
-		if(!$resultat){ 
-		?>
-			<div class="ui-widget">
-				<div class="ui-state-error ui-corner-all" style="padding: 0 .7em;"> 
-					<p>
-						<span class="ui-icon ui-icon-alert" 
-						    style="margin-right: .3em;"></span>
-						<strong>ERREUR: </strong> Login ou mot de passe incorrect !
-					</p>
-				</div>
-			</div>
+	
+	var_dump($_SESSION);
+	
+	if(!isset($_SESSION['id']) || !isset($_SESSION['nom']) || !isset($_SESSION['prenom']) || !isset($_SESSION['role'])){
+		if(empty($_POST['login']) || empty($_POST['mdp'])){
+					?>
+				
+			<form method="POST" action="login.php" id="loginForm">
+				<label>Nom d'utilisateur</label>
+				<input type="text" name="login"/>
+				<br/><br/>
+				<label>Mot de passe</label>
+				<input type="password" name="mdp"/>
+				<br/><br/><br/>
+				<button type="input">Connexion</button>
+			</form>
 		<?php
-		} 
-		else{ // Connexion réussi !
-			session_start();
-			$_SESSION['id'] = $resultat['id_u']; 
-			$_SESSION['nom'] = $resultat['nom'];
-			$_SESSION['prenom'] = $resultat['prenom'];
-			header('Location: index.php');
+			
+				if(isset($_POST['login']) || isset($_POST['mdp'])) 
+				{
+					?>
+					<div class="ui-widget">
+						<div class="ui-state-error ui-corner-all" style="padding: 0 .7em;"> 
+							<p>
+								<span class="ui-icon ui-icon-alert" 
+									style="margin-right: .3em;"></span>
+								<strong>ERREUR: </strong> L'un des 2 champs est vide !
+							</p>
+						</div>
+					</div>
+					<?php
+				}
+			}
+		else{
+			$req = $bdd->prepare('SELECT id_u, mail, mdp, nom, prenom, id_role FROM utilisateur WHERE mail = :mail AND mdp = :mdp');
+			$req->execute(array(
+			'mail' => $_POST['login'], 
+			'mdp' => sha1($_POST['mdp'])
+			));
+
+			$resultat = $req->fetch();
+
+			if(!$resultat){ 
+			?>
+				<div class="ui-widget">
+					<div class="ui-state-error ui-corner-all" style="padding: 0 .7em;"> 
+						<p>
+							<span class="ui-icon ui-icon-alert" 
+								style="margin-right: .3em;"></span>
+							<strong>ERREUR: </strong> Login ou mot de passe incorrect !
+						</p>
+					</div>
+				</div>
+			<?php
+			} 
+			else{ // Connexion réussi !
+				session_start();
+
+				$_SESSION['id'] = $resultat['id_u']; 
+				$_SESSION['nom'] = $resultat['nom'];
+				$_SESSION['prenom'] = $resultat['prenom'];
+				$_SESSION['role'] = $resultat['id_role'];
+
+				header('Location: index.php');
+			}
 		}
 	}
+	else header("Location: index.php");
 
 ?>
 	</div>
